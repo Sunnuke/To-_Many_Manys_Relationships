39646c6d1789955ac0a43fe7f3cb607ca2c94911
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-14 22:29:57 -0800

    Added spring-boot-devtools, mysql-connector-java, spring-boot-starter-tomcat, spring-boot-starter-validation, tomcat-embed-jasper, jstl dependencies to pom.xml

---
+++ manytomany/src/test/java/com/practice/manytomany/ManytomanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.manytomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManytomanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
