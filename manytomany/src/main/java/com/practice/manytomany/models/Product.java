10ecb8e6f45ae8e8f71377c993c18acaff0731e3
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-14 23:11:17 -0800

    Created (Model) Product class and (Model) Category class, for many to many relationship

---
+++ manytomany/src/main/java/com/practice/manytomany/models/Product.java
@@ -0,0 +1,114 @@
+package com.practice.manytomany.models;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Locale.Category;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="products")
+public class Product {
+    
+//	ATTRIBUTES
+	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    
+    private String description;
+    
+    private float price;
+    
+    @Column(updatable=false)
+    private Date createdAt;
+    
+    private Date updatedAt;
+    
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+        name = "categories_products", 
+        joinColumns = @JoinColumn(name = "product_id"), 
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private List<Category> categories;
+    
+//	Constructors    
+    
+    public Product() {
+        
+    }
+    
+//	PRESETS
+    
+	@PrePersist
+	private void onCreate() {
+		this.createdAt = new Date();
+	}
+	@PreUpdate
+	private void onUpdate() {
+		this.createdAt = new Date();
+	}
+
+//	METHODS
+    
+	// Id
+	public Long getId() {
+		return id;
+	}
+
+	// Name
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// Description
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	// Price
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	// Date/Time
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+// 	Relationship
+	// Many to Many
+	public List<Category> getCategories() {
+		return categories;
+	}
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
+	}
+    
+}
